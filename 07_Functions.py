ceb2d0aa8efbf91566fd0a239fe1d4d473f945bf
Connor <connormullett@gmail.com>
2018-10-12 13:34:31 -0400

    added functions files

---
+++ 07_Functions.py
@@ -0,0 +1,52 @@
+
+# functions are the 'verbs' of our programs
+# it is a block of organized and reusable code that performs a certain function
+# you should try to make your function do one thing and usually no more
+# we've already seen print(), print is a function. it prints something to the screen
+
+# to write a function we first need a new keyword, 'def'
+# 'def' means we are 'defining' a function
+
+students = []
+
+
+def add_student(name):
+    students.append(name)
+
+
+# notice the parenthesis before the colon, this is what we 'give' our function
+# we can have as many variables as we want in there, they are called parameters
+# so whenever we do add_student() and give it a name, it will add that name to our list
+# lets try it
+
+add_student('billy')
+print(students)
+
+# we can also pass in a variable instead of a hard coded string
+
+first_name = 'billy'
+add_student(first_name)
+print(students)
+
+# now we see billy twice if we run this
+# remember the parameters have to match, we can't only give our function 1 variable if we have
+# 2 parameters where we define it
+
+# now we're going to look at 'return' statements
+
+add_student('jimmy')
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student.title()
+    return students_titlecase  # the return means 'give this value back to whoever called the function
+
+
+# this means if we call the function get_students_titlecase() and assign it to a new variable
+# the RETURN will be the value assigned to the new variable
+
+student_list = get_students_titlecase()
+
+print(student_list)
---
+++ 08_Function_Arguments.py
@@ -0,0 +1,80 @@
+
+# in this lesson we are going to go over parameters/arguments
+# we're going to need our functions from our last lesson so lets write them out now
+
+students = []
+
+
+def add_student(name):
+    students.append(name)
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student.title()
+    return students_titlecase
+
+
+# but what if we want to make an argument optional, or not required
+# its easy! lets write a new function that will also assign an id to a student
+
+def add_student_with_id(name, student_id=332):  # note the =332
+    student = {'name': name, 'student_id': student_id}  # here's another dictionary, these are very useful
+    students.append(student)
+
+
+# on line 22 we see that we have set student_id to a hardcoded value of 332
+# this way we don't need to supply our function call with a student_id
+# lets see what that looks like
+
+add_student_with_id('connor')
+print(students)
+
+# if we run this we see that it works, even though our function looks like it wants a value for student_id
+# however we can also manually give it a value if we would like
+
+add_student_with_id('bill', 1)
+print(students)
+
+# if we run this we see that the value of 332 gets overridden by the value we gave when we called the function
+# we can actually do some pretty nifty stuff with our print function with arguments
+# try and figure this out
+
+print('hello', 'world', 3, None, 'something')
+
+# this is because the print function uses 'variable' arguments
+# basically it means that the parameters can 'vary' and don't have a set limit
+# lets dig into it
+
+
+def variable_arguments(name, *args):  # notice the asterisk
+    print(name)
+    print(args)  # no asterisk here
+
+
+variable_arguments('Jimmothy', None, 'Python is cool')
+# run this and see what happens
+# we see it captured our first argument and stored it in name, and printed it
+# then all of our other arguments get printed 2cnd
+# also notice that it placed the other arguments in a list
+# we can have as many arguments as we want now
+
+# now lets get into keyword arguments, what if we want to specify what the argument was?
+# in the last example we see that its just a big ole' list
+# we can use keyword arguments to convert it into a dictionary so we can sift through
+# key value pairs where the keys are the names of the arguments
+
+
+def keyword_args(name, **kwargs):  # notice the double * and KWargs (key word args)
+    print(name)
+    print(kwargs['description'], kwargs['type'])
+
+
+keyword_args('connor', description='a funny dude', type='person')
+# notice the red, it picked up that those words were part of the function
+# run this and see what happens
+
+# it replaces the kwargs['something in here'] with what we give it
+
+
