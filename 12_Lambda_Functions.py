720f34026e83f3ea1ae938624ff94f1d54c186e7
Connor <connormullett@gmail.com>
2018-10-15 11:48:00 -0400

    adding to lambda functions file

---
+++ 12_Lambda_Functions.py
@@ -0,0 +1,17 @@
+
+# lambda functions are anonymous functions that allow us to write short and simple functions
+# they can take arguments and get called the same way as a normal function
+# so what if we want a function that doubles a number, lets do that now
+
+
+def double(x):
+    return x * 2
+
+
+# easy right? lets make it even simpler
+
+
+double_lambda = lambda x: x * 2  # notice no paranthesis and the new 'lambda' keyword
+
+
+
